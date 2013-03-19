a567cb222644c49cdee803d2eb043cfc4459ee59
jonathan <jonathan@jonathan-rakete.(none)>
2013-03-19 18:27:57 +0100

    cleaned up a bit and prepared the event add function

--- index.php
+++ index.php
@@ -1,19 +1,16 @@
 <?php
-//this when project folder is /var/www/projectRoot
-$projectFolder = end(explode('/', getcwd()));
-//this when project folder is /var/www/
-// FIXME need to correct paths in menu, script and style
-//$projectFolder = "/"
+$projectFolder = "/"
 ?>
+
 <?php include 's.php'; ?>
 <!DOCTYPE HTML>
 <html>
 
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" href="/<? echo $projectFolder?>/css/main.css" type="text/css" media="screen" charset="utf-8">
+    <link rel="stylesheet" href="<? echo $projectFolder?>css/main.css" type="text/css" media="screen" charset="utf-8">
 
-    <title><? echo $projectFolder?> - <?=t()?></title>
+		<title>The Cypher - <?=t()?></title>
 
   </head>
   <body>
@@ -21,9 +18,9 @@ $projectFolder = end(explode('/', getcwd()));
     <h1><?=t()?></h1>
     <div id="menu">
       <ul id="menuList">
-          <li class="menuListEntry"><a href="/<? echo $projectFolder?>">Home</a></li>
-          <li class="menuListEntry"><a href="/<? echo $projectFolder?>/info">Info</a></li>
-          <li class="menuListEntry"><a href="/<? echo $projectFolder?>/break">breaks</a></li>
+          <li class="menuListEntry"><a href="<? echo $projectFolder?>">Home</a></li>
+          <li class="menuListEntry"><a href="<? echo $projectFolder?>info">Info</a></li>
+          <li class="menuListEntry"><a href="<? echo $projectFolder?>events">Events</a></li>
       </ul>
       
     </div>
@@ -32,6 +29,6 @@ $projectFolder = end(explode('/', getcwd()));
       <?=c()?>
     </div>
 
-    <script src="/<? echo $projectFolder?>/js/main.js" type="text/javascript" charset="utf-8"></script>
+    <script src="<? echo $projectFolder?>js/main.js" type="text/javascript" charset="utf-8"></script>
   </body>
 </html>
--- site/events.php
+++ site/events.php
@@ -2,18 +2,45 @@
 
 t('Events'); 
 
-class siteWithFunction {
+class events {
 
 	public $content = "";
 
+	//shows the map and the list of events
 	function index() {
-		$content = "index of events";
+		$this->content = <<<HTML
+		here should be a list of events... and a map
+<a href="events/add/">Add an event</a>
+HTML;
 		$this->write();
 	}
 
-	function foo() {
-		//get DB data
-		$this->content = "Db data recieved";
+	function add() {
+	//	var_dump($_POST);
+
+		$wasSaved = false;
+
+		if ($wasSaved) {
+			$this->content = <<<HTML
+<span>Your event was saved. Go back <a href="/">home</a></span>.
+HTML;
+		} else {
+		$this->content = <<<HTML
+<form action="." method="post">
+<input type="text" name="location" placeholder="location" value=""></input><br/>
+<input type="text" name="city" placeholder="city" value=""></input><br/>
+<input type="text" name="price" placeholder="price" value=""></input><br/>
+<input type="text" name="date" placeholder="date" value=""></input><br/>
+<input type="text" name="time" placeholder="time" value=""></input><br/>
+<input type="text" name="everyWeek" placeholder="everyWeek" value=""></input><br/>
+<input type="text" name="email" placeholder="email" value=""></input><br/>
+<input type="text" name="name" placeholder="name" value=""></input><br/>
+<input type="text" name="website" placeholder="website" value=""></input><br/>
+<input type="submit" name="submit" value="Submit"></input><br/>
+</form>
+HTML;
+		}
+
 		$this->write();
 	}
 
@@ -25,4 +52,3 @@ class siteWithFunction {
 }
 
 ?>
-
